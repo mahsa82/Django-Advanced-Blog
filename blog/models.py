ec8a38c2c54408a9fe94466001c03e96bd8c64fb
mahsa82 <mahsa.alizade8271@gmail.com>
2025-10-07 14:30:24 +0330

    some fix

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,29 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
     status = models.BooleanField()
-    category = models.ForeignKey(Category , on_delete=models.SET_NULL,null=True)
+    category = models.ForeignKey('Category' , on_delete=models.SET_NULL,null=True)
     image = models.ImageField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE)
     
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title
+    
+    class meta:
+        verbose_name ='پست'
+        verbose_name_plural='پست ها'
+        
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.name
+    
+    class meta:
+        verbose_name ='کتگوری'
+        verbose_name_plural='کتگوری ها'
+    
+    
