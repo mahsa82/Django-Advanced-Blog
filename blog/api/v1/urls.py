31845d1a22198407a441fd5de7b77d9eeb6496a6
mahsa82 <mahsa.alizade8271@gmail.com>
2025-10-15 13:54:07 +0330

    default router

--- blog/api/v1/urls.py
+++ blog/api/v1/urls.py
@@ -2,12 +2,22 @@ from django.urls import path
 from . import views
 from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
+from rest_framework.routers import DefaultRouter
+
+router =DefaultRouter()
+router.register('post',views.PostViewSet,basename='post')
 
 app_name = "api-v1"
 
-urlpatterns = [
-    # path('post/',views.PostList, name="post-list"),
-    # path('post/<int:id>/',views.postDetail , name="post-detail"),
-    path('post/',views.PostList.as_view() , name="post-list"),
-    path('post/<int:pk>/',views.PostDetail.as_view() , name="post-detail"),
-]
+urlpatterns = router.urls
+
+# urlpatterns = [
+#      path('post/',views.PostList, name="post-list"),
+#      path('post/<int:id>/',views.postDetail , name="post-detail"),
+#      path('post/',views.PostList.as_view() , name="post-list"),
+#      path('post/<int:pk>/',views.PostDetail.as_view() , name="post-detail"),
+#      path('post/',views.PostViewSet.as_view({'get':'list','post':'create'}),name='post-list'),
+#      path('post/<int:pk>/',views.PostViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}),name='post-list'),
+#      path('',include(router.urls)),
+# ]
+#urlpatterns += router.urls
