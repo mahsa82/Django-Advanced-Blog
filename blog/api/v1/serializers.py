267ff3bd48b94cd70ec143a8b0ca41c613dee749
mahsa82 <mahsa.alizade8271@gmail.com>
2025-10-18 12:28:28 +0330

    function base model

--- blog/api/v1/serializers.py
+++ blog/api/v1/serializers.py
@@ -6,10 +6,16 @@ from ...models import Post,Category
 #     title = serializers.CharField(max_length=255)
     
 class PostSerializer(serializers.ModelSerializer):
-    
+    snippet = serializers.ReadOnlyField(source='get_snippet')
+    relative_url = serializers.URLField(source='get_absolute_api_url',read_only=True)
+    absolute_url = serializers.SerializerMethodField()
     class Meta :
         model = Post
-        fields = ['id','author','title','content','category','status','created_date','published_date']    
+        fields = ['id','author','title','content','snippet','category','status','relative_url','absolute_url','created_date','published_date']    
+    
+    def get_absolute_url(self,obj):
+        request = self.context.get('request')
+        return request.build_absolute_url(obj,pk)
         
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
