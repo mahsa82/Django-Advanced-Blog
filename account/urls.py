f706ce48f5a15d1dcf669bb2cb734e3e0bb35c6f
mahsa82 <mahsa.alizade8271@gmail.com>
2025-11-02 18:19:39 +0330

    add v2

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,6 @@ app_name = 'account'
 urlpatterns = [
     path('',include('django.contrib.auth.urls')),
     path('api/v1/',include('account.api.v1.urls')),
+    path('api/v2/',include('djoser.urls')),
+    path('api/v2/',include('djoser.urls.jwt')),
 ]
